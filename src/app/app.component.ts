329cb5dbd30fc00690cd30893b83605e2d2dc094
AmpF5 <lolc44t@gmail.com>
2023-11-25 23:14:56 +0100

    init + firebasesetup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Firestore, collection, addDoc, collectionData } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'quiz-maker';
+  constructor(private firestore: Firestore, private cd: ChangeDetectorRef) {
+  }
+
+
+
+
+  protected getData() {
+    const collectionInstance = collection(this.firestore, 'users');
+    collectionData(collectionInstance)
+    .subscribe({
+      next: value => {
+        console.log(value);
+      },
+      error: err => {
+        console.error(err);
+      }
+    });
+  }
+
+
+  protected addData(form: NgForm) {
+    const collectionInstance = collection(this.firestore, 'users');
+    addDoc(collectionInstance, form.value)
+    .then(() => {
+      console.log('git gut fajny ten firebase');
+    })
+    .catch(err => {
+      console.error(err);
+    });
+  }
 }
